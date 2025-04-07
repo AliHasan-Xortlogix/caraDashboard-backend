63ff034346dfa020435e529cdd6402f7c49a3cd4
Ali-hasan <alihassan.xortlogix@gmail.com>
2025-04-08 01:42:42 +0500

    Update Gallery.routes.js
    xxx

--- routes/Gallery.routes.js
+++ routes/Gallery.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getContactsWithCustomFields } = require('../controllers/Gallery.Controller');
+const { getContactsWithCustomFields, getSingleContact } = require('../controllers/Gallery.Controller');
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/jwtToken');
 const { getSuggestion } = require('../controllers/suggestion.Controller');
 // Define route for getting all contacts with custom fields and display settings
 router.get('/galleryview', isAuthenticatedUser,getContactsWithCustomFields);
+router.get('/contactsview', getSingleContact);
 router.get('/search-suggestions', getSuggestion)
 module.exports = router;
