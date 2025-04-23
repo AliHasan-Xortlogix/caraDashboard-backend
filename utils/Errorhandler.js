177343264d572199944a4aefe887b69297691176
Ali-hasan <alihassan.xortlogix@gmail.com>
2025-04-24 02:05:42 +0500

    Update Errorhandler.js
    erro

--- utils/Errorhandler.js
+++ utils/Errorhandler.js
@@ -1,7 +1,10 @@
+// ErrorHandler.js (you already have this but make sure it's proper)
 class ErrorHandler extends Error {
     constructor(message, statusCode) {
         super(message);
-        this.statusCode = statusCode
+        this.statusCode = statusCode;
+
+        Error.captureStackTrace(this, this.constructor);
     }
 }
-module.exports = ErrorHandler
+module.exports = ErrorHandler;
